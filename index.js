07bf1c8b97b6f4aef317924b27b5effc82dca7f9
retamalesn <retamalesn@gmail.com>
2018-05-28 15:46:11 -0300

    console output errors added

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var xsd = require('libxml-xsd');
 
 const xml = require('fs').readFileSync('test.xml', 'utf-8')
- 
-xsd.parseFile('matrXmlSchema.xsd', function(err, schema){
-  schema.validate(xml, function(err, validationErrors){
-  		if(validationErrors != null)
-  			console.log(validationErrors);
-  		else
-  			console.log('No hay errores');
-  });  
+
+xsd.parseFile('matrXmlSchema.xsd', function(err, schema) {
+    schema.validate(xml, function(err, validationErrors) {
+        if (validationErrors != null){
+            for (var value of validationErrors) {
+            	var arrayOfStrings = value.toString().split('.');
+            	console.log(arrayOfStrings[0]);
+            	if(arrayOfStrings[0].includes('is not expected')){
+            		console.log('Hay un elemento no permitido');
+            	}
+            }
+        }else{
+            console.log('No hay errores');
+        }
+    });
 });
